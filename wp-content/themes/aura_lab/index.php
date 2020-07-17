8416dc165ddde47d569c6f7b7b830809d469c15d
Guilherme Roberto <guilherme.rfs00@gmail.com>
2020-07-17 15:23:54 -0300

    Index criado

---
+++ wp-content/themes/aura_lab/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aura Provocativa</title>
+</head>
+
+<body <?php body_class(); ?>>
+    <div id="page" class="site">
+        <header>
+            <section class="search">Pesquisa</section>
+            <section class="top-bar">
+                <div class="brand">Logo</div>
+                <div class="second-column">
+                    <div class="account">Conta</div>
+                    <nav class="main-menu">Menu</nav>
+                </div>
+            </section>
+        </header>
+
+        <div class="content-area">
+            <main>
+                <section class="slider">Slider</section>
+                <section class="popular-products">Produtos populares</section>
+                <section class="new-arrivals">Lançamentos</section>
+                <section class="deal-of-the-week">Promoção da semana</section>
+                <section class="lab-blog">Notícias</section>
+            </main>
+        </div>
+        <footer>
+            <section class="footer-widgets">Widgets do rodapé</section>
+            <section class="copyrigth">Copyrigth</section>
+        </footer>
+    </div>
+</body>
+
+</html>
