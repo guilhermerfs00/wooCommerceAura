4095212dbcbbf86db91064ee91875b7430414feb
Guilherme Roberto <guilherme.rfs00@gmail.com>
2020-07-17 15:39:22 -0300

    Separando os arquivos

---
+++ wp-content/themes/aura_lab/footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <section class="footer-widgets">Widgets do rodapé</section>
+    <section class="copyrigth">Copyrigth</section>
+</footer>
+</div>
+</body>
+
+</html>
---
+++ wp-content/themes/aura_lab/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta <?php bloginfo('charset'); ?>>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="profile" href="https://gmpg.org/xfn/11" />
+</head>
+
+<body <?php body_class(); ?>>
+    <div id="page" class="site">
+        <header>
+            <section class="search">Pesquisa</section>
+            <section class="top-bar">
+                <div class="brand">Logo</div>
+                <div class="second-column">
+                    <div class="account">Conta</div>
+                    <nav class="main-menu">Menu</nav>
+                </div>
+            </section>
+        </header>
